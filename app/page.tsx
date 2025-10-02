39af2fd2ac8a3aac3a7dc985ccac0463b619c4d8
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-02 18:34:08 +0530

    chore: setup external dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       Hello LingoPy Editor
+
+      <Button>Run</Button>
     </div>
   );
 }
