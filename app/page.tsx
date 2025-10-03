862057337597da7ab268115a37240469c06b3235
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-03 10:59:19 +0530

    feat: add basic text editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import AppBar from "@/components/app-bar";
-import Editor from "@/components/editor";
+import EditorComponent from "@/components/editor";
 import Output from "@/components/output";
 
 export default function Home() {
   return (
     <div>
       <AppBar />
-      <Editor />
+      <EditorComponent />
       <Output />
     </div>
   );
--- components/editor.tsx
+++ components/editor.tsx
@@ -1,7 +1,22 @@
-export default function Editor() {
-    return (
-        <div className="p-4">
-            Editor Component
-        </div>
-    )
+"use client";
+
+import { useState } from "react";
+import { Editor } from "primereact/editor";
+
+export default function EditorComponet() {
+  const [text, setText] = useState("");
+  return (
+    <div className="p-4 flex flex-col">
+      <Editor
+        value={text}
+        onTextChange={(e) => setText(e.htmlValue || "")}
+        style={{
+          minHeight: "200px",
+          maxHeight: "320px",
+          height: "320px",
+        }}
+        className="sm:min-h-[200px] sm:max-h-[320px] md:min-h-[300px] md:max-h-[480px] lg:min-h-[400px] lg:max-h-[600px]"
+      />
+    </div>
+  );
 }
