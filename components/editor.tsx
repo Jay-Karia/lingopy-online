880835beb7b01d0deeee1460dfaef216264b1276
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-03 11:11:02 +0530

    chore: adjust editor heights

--- components/editor.tsx
+++ components/editor.tsx
@@ -6,16 +6,12 @@ import { Editor } from "primereact/editor";
 export default function EditorComponet() {
   const [text, setText] = useState("");
   return (
-    <div className="p-4 flex flex-col">
+    <div className="p-4 flex flex-col h-[50vh] sm:h-[70vh] overflow-scroll">
       <Editor
         value={text}
         onTextChange={(e) => setText(e.htmlValue || "")}
-        style={{
-          minHeight: "200px",
-          maxHeight: "320px",
-          height: "320px",
-        }}
-        className="sm:min-h-[200px] sm:max-h-[320px] md:min-h-[300px] md:max-h-[480px] lg:min-h-[400px] lg:max-h-[600px]"
+        className="flex-1 min-h-0"
+        style={{ height: '100%' }}
       />
     </div>
   );
