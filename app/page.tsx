4c07751c5cb7ab68bf0dd0edcb2fd46ab2d11411
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-07 22:41:55 +0530

    chore: add made with love footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import AppBar from "@/components/app-bar";
 import EditorComponent from "@/components/editor";
 import Output from "@/components/output";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative min-h-screen">
       <AppBar />
       <EditorComponent />
       <Output />
+      <footer className="fixed bottom-2 right-2 text-sm text-muted-foreground">
+        Made with 💗 by <Link href="https://github.com/Jay-Karia" target="_blank">Jay Karia</Link> and <Link href="https://github.com/shaunbenedict" target="_blank">
+        S. Shaun Benedict</Link>
+      </footer>
     </div>
   );
 }
